4728382fabe820a6d0b160fff47906970fecc062
Zach Grace <ztgrace@gmail.com>
2018-01-21 22:35:13 -0600

    Fixing EOF error with ReadBytes

--- masks.go
+++ masks.go
@@ -3,15 +3,16 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"os"
+	"io"
 	"log"
-	"unicode"
+	"os"
 	"sort"
+	"unicode"
 )
 
 func main() {
 
-	var count int64 = 0
+	var count int64 = 1
 	input := os.Args[1]
 	masks := make(map[string]int)
 	fileHandle, err := os.Open(input)
@@ -19,13 +20,18 @@ func main() {
 		log.Fatal(input, " file not found")
 	}
 	defer fileHandle.Close()
-	fileScanner := bufio.NewScanner(fileHandle)
-	fileScanner.Split(bufio.ScanLines)
+	fileReader := bufio.NewReader(fileHandle)
 
-	for fileScanner.Scan() {
-		text := fileScanner.Text()
-		mask := getMask(text)
-		//fmt.Printf("%v,%v\n", text, mask)
+	for {
+		bytes, err := fileReader.ReadBytes('\n')
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			log.Printf("Error processing %v", count)
+			log.Printf("Error %v", err)
+		}
+		mask := getMask(bytes)
 		masks[mask] += 1
 		count += 1
 	}
@@ -33,11 +39,12 @@ func main() {
 	sortedMasks := rankByCount(masks)
 	printTopMasks(sortedMasks, 10)
 
-	fmt.Printf("Processed %v words", count)
+	fmt.Printf("Processed %v words\n", count)
 }
 
-func getMask(text string) (mask string) {
+func getMask(text []byte) (mask string) {
 	for _, char := range text {
+		char := rune(char)
 		if unicode.IsDigit(char) {
 			mask += "d"
 		} else if unicode.IsUpper(char) {
@@ -65,10 +72,10 @@ func printTopMasks(masks PairList, num int) {
 
 // https://stackoverflow.com/questions/18695346/how-to-sort-a-mapstringint-by-its-values
 
-func rankByCount(masks map[string]int) PairList{
+func rankByCount(masks map[string]int) PairList {
 	pl := make(PairList, len(masks))
 	i := 0
-	for k,v := range masks {
+	for k, v := range masks {
 		pl[i] = Pair{k, v}
 		i++
 	}
@@ -77,12 +84,12 @@ func rankByCount(masks map[string]int) PairList{
 }
 
 type Pair struct {
-	Key string
+	Key   string
 	Value int
 }
 
 type PairList []Pair
 
-func (p PairList) Len() int { return len(p) }
+func (p PairList) Len() int           { return len(p) }
 func (p PairList) Less(i, j int) bool { return p[i].Value < p[j].Value }
-func (p PairList) Swap(i, j int){ p[i], p[j] = p[j], p[i] }
+func (p PairList) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
